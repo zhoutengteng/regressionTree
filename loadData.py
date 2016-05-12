d63286cd5304a84d4ce55e234de87c4654e2a605
周腾腾 <zhoutengteng@MacBook-Pro-zhou.local>
2016-05-12 17:36:35 +0800

    z自己产生随机数据的文档

---
+++ loadData.py
@@ -0,0 +1,17 @@
+import random
+import numpy as np
+
+def produceData():
+    data = 500
+    list = []
+    label1 = map(lambda value: (value, random.randint(1, data), 1), map(lambda value: random.randint(1, data), range(0, data)))
+    label2 = map(lambda value: (value, random.randint(1, data), 2), map(lambda value: -1 * random.randint(1, data), range(0, data)))
+    label3 = map(lambda value: (value, -1 * random.randint(1, data), 3), map(lambda value: -1 * random.randint(1, data), range(0, data)))
+    label4 = map(lambda value: (value, -1 * random.randint(1, data), 4), map(lambda value: random.randint(1, data), range(0, data)))
+
+    list.extend(label1)
+    list.extend(label2)
+    list.extend(label3)
+    list.extend(label4)
+    random.shuffle(list)
+    return np.array(list)
